f0afe6cfa4f9a70230175436927d612337b6a813
Liam Keegan <liam@keegan.ch>
2021-07-14 21:30:16 +0200

    expand %T

--- main.cc
+++ main.cc
@@ -10,6 +10,6 @@ int main() {
                            std::chrono::system_clock::now());
     std::tm local_time = *std::localtime(&startup_time);
 
-  auto time_string = fmt::format("{:%a %Y-%m-%d %T %Z}",local_time);
+  auto time_string = fmt::format("{:%a %Y-%m-%d %H:%M:%S %Z}",local_time);
   fmt::print(time_string);
 }
